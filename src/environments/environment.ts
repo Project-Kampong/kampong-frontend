de0349a7caec592ebf7fa3b3a4c6ddeee865cbe0
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-08-25 00:03:27 +0800

    Changed environment variables for dev to localhost. and prod to use https://kampong.app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrl: "https://kampong.app/",
+  production: false,
+  apiUrl: "http://localhost:5000/",
 };
 
 /*
