2862cb8b1b730b16973309da6be0b5a1b1475d25
Darren Sim <klassiclegend@gmail.com>
2020-09-12 00:07:42 +0800

    Do up landing page

--- src/app/components/main-search/main-search.component.ts
+++ src/app/components/main-search/main-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { ListingsService } from '@app/services/listings.service';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { Location } from "@angular/common";
 declare var $: any;
 
 @Component({
@@ -6,7 +9,155 @@ declare var $: any;
     templateUrl: "./main-search.component.html",
     styleUrls: ["./main-search.component.scss"],
 })
-export class MainSearchComponent {
+export class MainSearchComponent implements OnInit {
 
-    constructor() {}
+    searchParams: FormGroup;
+
+    constructor(
+        private fb: FormBuilder,
+        ) { }
+
+    locationList = [
+        {
+            name: "North",
+            group: [
+            "Admirality",
+            "Kranji",
+            "Woodlands",
+            "Sembawang",
+            "Yishun",
+            "Yio Chu Kang",
+            "Seletar",
+            "Sengkang",
+            ],
+        },
+        {
+            name: "South",
+            group: [
+            "Holland",
+            "Queenstown",
+            "Bukit Merah",
+            "Telok Blangah",
+            "Pasir Panjang",
+            "Sentosa",
+            "Bukit Timah",
+            "Newton",
+            "Orchard",
+            "City",
+            "Marina South",
+            ],
+        },
+        {
+            name: "East",
+            group: [
+            "Serangoon",
+            "Punggol",
+            "Hougang",
+            "Tampines",
+            "Pasir Ris",
+            "Loyang",
+            "Simei",
+            "Kallang",
+            "Katong",
+            "East Coast",
+            "Macpherson",
+            "Bedok",
+            "Pulau Ubin",
+            "Pulau Tekong",
+            ],
+        },
+        {
+            name: "West",
+            group: [
+            "Lim Chu Kang",
+            "Choa Chu Kang",
+            "Bukit Panjang",
+            "Tuas",
+            "Jurong East",
+            "Jurong West",
+            "Jurong Industrial Estate",
+            "Bukit Batok",
+            "Hillview",
+            "West Coast",
+            "Clementi",
+            ],
+        },
+        {
+            name: "Central",
+            group: [
+            "Thomson",
+            "Marymount",
+            "Sin Ming",
+            "Ang Mo Kio",
+            "Bishan",
+            "Serangoon Gardens",
+            "MacRitchie",
+            "Toa Payoh",
+            ],
+        },
+        ];
+    categoryList = [
+        {
+            name: "Social",
+            group: [
+            "Health",
+            "Marriage",
+            "Education",
+            "Mentorship",
+            "Retirement",
+            "Housing",
+            "Rental Flats",
+            "Family",
+            "Gender",
+            "Elderly",
+            "Youth",
+            "Youth At Risk",
+            "Pre-School",
+            "Race",
+            "Language",
+            "Science",
+            "Art",
+            "Sports",
+            "Poverty",
+            "Inequality",
+            ],
+        },
+        {
+            name: "Environment",
+            group: ["Recycling", "Green", "Water", "Waste", "Food", "Growing"],
+        },
+        {
+            name: "Economical",
+            group: [
+            "Finance",
+            "Jobs",
+            "Wage",
+            "Upskill",
+            "Technology ",
+            "IT",
+            "IoT 4.0",
+            "Information",
+            "Automation",
+            "Online",
+            "Digitalization",
+            ],
+        },
+        {
+            name: "Others",
+            group: ["Productivity", "Innovation", "Research", "Manpower", "Design"],
+        }
+    ]
+
+    ngOnInit() {
+        this.searchParams = this.fb.group({
+            nameParams: new FormControl(''),
+            locationParams: [],
+            categoryParams: []
+        })
+    }
+
+    initiateSearch() {
+        console.log(this.searchParams.value);
+    }
+
 }
