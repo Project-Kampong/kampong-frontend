bc19a08c95dc6bd73e8690bc7bea30b668c78824
derrickSP <derricklimkaihao7.17@ichat.sp.edu.sg>
2020-09-25 18:04:04 +0800

    listing home revamp

--- src/app/pages/listing-home/listing-home.component.ts
+++ src/app/pages/listing-home/listing-home.component.ts
@@ -14,6 +14,57 @@ export class ListingHomeComponent implements OnInit {
     public ListingsService: ListingsService,
     public AuthService: AuthService
   ) {}
+  categoryGroup = [
+    {
+      name: "Social",
+      group: [
+        "Health",
+        "Marriage",
+        "Education",
+        "Mentorship",
+        "Retirement",
+        "Housing",
+        "Rental Flats",
+        "Family",
+        "Gender",
+        "Elderly",
+        "Youth",
+        "Youth At Risk",
+        "Pre-School",
+        "Race",
+        "Language",
+        "Science",
+        "Art",
+        "Sports",
+        "Poverty",
+        "Inequality",
+      ],
+    },
+    {
+      name: "Environment",
+      group: ["Recycling", "Green", "Water", "Waste", "Food", "Growing"],
+    },
+    {
+      name: "Economical",
+      group: [
+        "Finance",
+        "Jobs",
+        "Wage",
+        "Upskill",
+        "Technology ",
+        "IT",
+        "IoT 4.0",
+        "Information",
+        "Automation",
+        "Online",
+        "Digitalization",
+      ],
+    },
+    {
+      name: "Others",
+      group: ["Productivity", "Innovation", "Research", "Manpower", "Design"],
+    },
+  ];
 
   ngOnInit() {
     window.scroll(0, 0);
@@ -23,5 +74,9 @@ export class ListingHomeComponent implements OnInit {
     });
     // Get Public Listing
     this.ListingsService.getListingLoop(1);
+
+    $(".category-filter ul li .outline").on("click", function () {
+      $(this).toggleClass("active");
+    });
   }
 }
