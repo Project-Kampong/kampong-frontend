65cbc63064ba7f6ca1e0a72aacd3e0d7409f3936
angrybunny123 <kennethteo98@gmail.com>
2020-12-25 23:12:59 +0800

    Fix merge conflicts

--- src/app/pages/onboarding/onboarding.component.ts
+++ src/app/pages/onboarding/onboarding.component.ts
@@ -1,116 +1,106 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, ValidationErrors } from "@angular/forms";
-import { Router } from "@angular/router";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, ValidationErrors } from '@angular/forms';
+import { Router } from '@angular/router';
 
 // Services
-import { AuthService } from "@app/services/auth.service";
-import { ProfileService } from "@app/services/profile.service";
-import { SnackbarService } from "@app/services/snackbar.service";
+import { AuthService } from '@app/services/auth.service';
+import { ProfileService } from '@app/services/profile.service';
+import { SnackbarService } from '@app/services/snackbar.service';
 // Interface
-import { Profile } from "@app/interfaces/profile";
-import { profileForm } from "@app/util/forms/profile";
-import { UserData } from "@app/interfaces/user";
-import { Subscription } from "rxjs";
+import { Profile } from '@app/interfaces/profile';
+import { profileForm } from '@app/util/forms/profile';
+import { UserData } from '@app/interfaces/user';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-onboarding",
-  templateUrl: "./onboarding.component.html",
-  styleUrls: ["./onboarding.component.scss"],
+  selector: 'app-onboarding',
+  templateUrl: './onboarding.component.html',
+  styleUrls: ['./onboarding.component.scss'],
 })
 export class OnboardingComponent implements OnInit, OnDestroy {
-  
   private userData: UserData = <UserData>{};
   editProfileForm: FormGroup;
   profileData: Profile = <Profile>{};
   isLoggedIn: boolean = false;
   subscriptions: Subscription[] = [];
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private profileService: ProfileService, private router: Router, private snackbarService: SnackbarService) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private profileService: ProfileService,
+    private router: Router,
+    private snackbarService: SnackbarService,
+  ) {}
 
   ngOnInit() {
     this.editProfileForm = this.fb.group({ ...profileForm });
     if (this.authService.checkCookie()) {
-      this.subscriptions.push(this.authService.getUserDataByToken().subscribe(
-        (res) => {
-          this.userData = res["data"];
-          this.subscriptions.push(this.profileService.getUserProfile(this.userData["user_id"]).subscribe(
-            (res) => {
-              this.profileData = res["data"];
-              this.editProfileForm.patchValue(this.profileData);
-              this.isLoggedIn = true;
-            },
-            (err) => {
-              console.log(err);
-            }
-          ))
-        },
-        (err) => {
-          console.log(err);
-          console.log("User is not logged in");
-          this.router.navigate(["/login"]);
-        }
-      ))
+      this.subscriptions.push(
+        this.authService.getUserDataByToken().subscribe(
+          (res) => {
+            this.userData = res['data'];
+            this.subscriptions.push(
+              this.profileService.getUserProfile(this.userData['user_id']).subscribe(
+                (res) => {
+                  this.profileData = res['data'];
+                  this.editProfileForm.patchValue(this.profileData);
+                  this.isLoggedIn = true;
+                },
+                (err) => {
+                  console.log(err);
+                },
+              ),
+            );
+          },
+          (err) => {
+            console.log(err);
+            console.log('User is not logged in');
+            this.router.navigate(['/login']);
+          },
+        ),
+      );
     }
   }
 
   saveProfile() {
     if (this.getFormValidationErrors()) {
-      this.snackbarService.openSnackBar(
-        this.snackbarService.DialogList.setup_profile.validation_error,
-        false
-      );
+      this.snackbarService.openSnackBar(this.snackbarService.DialogList.setup_profile.validation_error, false);
       return;
     }
-    this.profileService.updateUserProfile(
-      this.profileData["user_id"],
-      this.editProfileForm.value,
-      this.authService.getAuthOptions()
-    ).subscribe(
+    this.profileService.updateUserProfile(this.profileData['user_id'], this.editProfileForm.value).subscribe(
       (res) => {
         if (true) {
           var ImageFd = new FormData();
-          this.profileService.updateUserProfilePic(
-            this.profileData["user_id"],
-            ImageFd,
-            this.authService.getAuthOptionsWithoutContentType()
-          ).subscribe(
+          this.profileService.updateUserProfilePic(this.profileData['user_id'], ImageFd).subscribe(
             (res) => {
-              this.router.navigate(["/profile"]);
+              this.router.navigate(['/profile']);
             },
             (err) => {
-              console.log("error");
-            }
+              console.log('error');
+            },
           );
         } else {
           //this.authService.LoginResponse.emit();
-          this.snackbarService.openSnackBar(
-            this.snackbarService.DialogList.setup_profile.success,
-            true
-          );
-          this.router.navigate(["/profile"]);
+          this.snackbarService.openSnackBar(this.snackbarService.DialogList.setup_profile.success, true);
+          this.router.navigate(['/profile']);
         }
       },
       (err) => {
-        console.log("error");
-        this.snackbarService.openSnackBar(
-          this.snackbarService.DialogList.setup_profile.error,
-          false
-        );
-        this.router.navigate(["/profile"]);
-      }
+        console.log('error');
+        this.snackbarService.openSnackBar(this.snackbarService.DialogList.setup_profile.error, false);
+        this.router.navigate(['/profile']);
+      },
     );
   }
 
   uploadFile(e: any): void {
-    console.log("Upload file");
+    console.log('Upload file');
   }
-
 
   getFormValidationErrors() {
     var error = false;
     Object.keys(this.editProfileForm.controls).forEach((key) => {
-      const controlErrors: ValidationErrors = this.editProfileForm.get(key)
-        .errors;
+      const controlErrors: ValidationErrors = this.editProfileForm.get(key).errors;
       if (controlErrors != null) {
         Object.keys(controlErrors).forEach((keyError) => {
           error = true;
@@ -121,6 +111,6 @@ export class OnboardingComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
